4900571c004b3ffb4e94bf83369fcb6825e277c7
Karine Sousa <ksousa27@gmail.com>
2018-12-08 19:28:41 -0300

    1

--- Trabalho-2/js/main.js
+++ Trabalho-2/js/main.js
@@ -1,37 +1,122 @@
-// let url = "http://rem-rest-api.herokuapp.com/"
+//url servidor
 
-// let reqTeste = new XMLHttpRequest();
+let url = "http://rest.learncode.academy/api/kau/lms";
 
+function postGrupos(){
+  let xhttp = new XMLHttpRequest();
+  
+  xhttp.onreadystatechange = function(){
+      if(xhttp.readyState==4){
+          console.log(xhttp.responseText);
+      }
+  }
 
-// function adicionarGrupo(){
-//     let grupos = document.querySelector(".grupos");
+  xhttp.open("POST", url, true);
+  xhttp.setRequestHeader("Content-type","application/json");
 
-//     let grupo = document.createElement("div");
-//     grupo.classList.add("grupo");
-
-//     let imgGrupo = document.createElement("div");
-//     imgGrupo.classList.add("imgGrupo");
-    
-//     let tituloGrupo = document.createElement("span");
-//     tituloGrupo.classList.add("tituloGrupo");
-
-//     let textoTeste = document.createTextNode("blabla");
-
-//     tituloGrupo.appendChild(textoTeste);
-
-//     grupos.appendChild(grupo);
-//     grupo.appendChild(imgGrupo);
-//     grupo.appendChild(tituloGrupo);
-// }
-
-// adicionarGrupo();
-// adicionarGrupo();
-// adicionarGrupo();
-
-var xhr = new XMLHttpRequest()
-xhr.open("GET", "http://rem-rest-api.herokuapp.com/api/users", true)
-xhr.withCredentials = true
-xhr.send()
-xhr.onload = function() {
-  var data = JSON.parse(xhr.responseText)
+  let msg = [
+      //GRUPO 1
+      { 
+      grupo: "Grupo da família!",
+      mensagens: [{
+          usuario: "joao03",
+          texto: "Tudo bem?"
+          },
+          {
+          usuario: "victor23",
+          texto: "Tudo Tranqs"
+          },
+          {
+          usuario: "joao03",
+          texto: "Que bom"
+          },
+      ]
+      },
+      //GRUPO 2
+      { 
+      grupo: "Churrascão no domingão",
+      mensagens: [{
+          usuario: "maria2000",
+          texto: "Na paz?"
+          },
+          {
+          usuario: "victor23",
+          texto: "Show"
+          },
+          {
+          usuario: "maria2000",
+          texto: "Que bom"
+          },
+      ]
+      },
+      //GRUPO 3
+      { 
+      grupo: "Só topzera",
+      mensagens: [{
+          usuario: "victor03",
+          texto: "Bom?"
+          },
+          {
+          usuario: "robson_alves",
+          texto: "Bom"
+          },
+          {
+          usuario: "victor03",
+          texto: "Que bom"
+          },
+      ]
+      }
+  ];
+  xhttp.send(JSON.stringify(msg));
 }
+
+function getGrupos(){
+  let xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function(){
+      if(xhttp.readyState==4){
+          let body_parsed = JSON.parse(xhttp.responseText);
+          console.log(xhttp.responseText);
+          console.log(body_parsed);
+      }
+  }
+  xhttp.open("GET", url, true);
+  xhttp.send();
+}
+
+function criarGrupo(){
+  let xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function(){
+      if(xhttp.readyState==4){
+          let body_parsed = JSON.parse(xhttp.responseText);
+          console.log(xhttp.responseText);
+          console.log(body_parsed[0]);
+      }
+  }
+  xhttp.open("GET", url, true);
+  xhttp.send();
+
+  //criando elementos
+  let grupos = document.querySelector(".grupos");
+
+  let grupo = document.createElement("div");
+  grupo.classList.add("grupo");
+
+  let imgGrupo = document.createElement("div");
+  imgGrupo.classList.add("imgGrupo");
+  
+  let tituloGrupo = document.createElement("span");
+  tituloGrupo.classList.add("tituloGrupo");
+
+  // juntando elementos
+  let textoTeste = document.createTextNode("blabla");
+
+  tituloGrupo.appendChild(textoTeste);
+
+  grupo.appendChild(imgGrupo);
+  grupo.appendChild(tituloGrupo);
+  grupos.appendChild(grupo);
+}
+
+postGrupos();
+getGrupos();
+criarGrupo();
