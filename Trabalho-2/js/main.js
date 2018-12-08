4a7db44a8577bec10919602c67ec83b8a6dbf91d
Karine Sousa <ksousa27@gmail.com>
2018-12-08 20:53:55 -0300

    commit

--- Trabalho-2/js/main.js
+++ Trabalho-2/js/main.js
@@ -88,39 +88,44 @@ function getGrupos(){
 
 function criarGrupo(){
   let xhttp = new XMLHttpRequest();
+
   xhttp.onreadystatechange = function(){
       if(xhttp.readyState==4){
-          let body_parsed = JSON.parse(xhttp.responseText);
-          console.log(xhttp.responseText);
-          
-          console.log(body_parsed[0][1].grupo);
+          console.log("entrou");
+          function nomeGrupo(nome){
+            let grupos = document.querySelector(".grupos");
+        
+            let grupu = document.createElement("div");
+            grupu.classList.add("grupo");
+        
+            let imgGrupo = document.createElement("div");
+            imgGrupo.classList.add("imgGrupo");
+            
+            let tituloGrupo = document.createElement("span");
+            tituloGrupo.classList.add("tituloGrupo");
+        
+            // juntando elementos
+            let textoTeste = document.createTextNode(nome);
+        
+            tituloGrupo.appendChild(textoTeste);
+        
+            grupu.appendChild(imgGrupo);
+            grupu.appendChild(tituloGrupo);
+            grupos.appendChild(grupu);
+          }
+
+          let infoGrupo = JSON.parse(xhttp.responseText);
+          let grupo0 = infoGrupo[0][0].grupo;
+          let grupo1 = infoGrupo[0][1].grupo;
+          let grupo2 = infoGrupo[0][2].grupo;
+          nomeGrupo(grupo0);
+          nomeGrupo(grupo1);
+          nomeGrupo(grupo2);
+
       }
   }
   xhttp.open("GET", url, true);
   xhttp.send();
-
-  //criando elementos
-  let grupos = document.querySelector(".grupos");
-
-  let grupu = document.createElement("div");
-  grupu.classList.add("grupo");
-
-  let imgGrupo = document.createElement("div");
-  imgGrupo.classList.add("imgGrupo");
-  
-  let tituloGrupo = document.createElement("span");
-  tituloGrupo.classList.add("tituloGrupo");
-
-  // juntando elementos
-  let textoTeste = document.createTextNode("blabla");
-
-  tituloGrupo.appendChild(textoTeste);
-
-  grupu.appendChild(imgGrupo);
-  grupu.appendChild(tituloGrupo);
-  grupos.appendChild(grupu);
-
-  console.log(grupos);
 }
 
 postGrupos();
